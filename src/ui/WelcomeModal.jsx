1d7518783f052f853d77c44f50bc212258ba7f6f
J vanBemmel <jvb127@gmail.com>
2021-04-26 21:42:24 -0500

    Update WelcomeModal.jsx

--- src/ui/WelcomeModal.jsx
+++ src/ui/WelcomeModal.jsx
@@ -8,6 +8,7 @@ import Modal from "ui/common/Modal";
 import Button from "ui/common/Button";
 import Anchor from "ui/common/Anchor";
 import tilesetSample from "resources/tileset_sample.png";
+import tilesetSample2 from "resources/tilesets_deviant_milkian_1.png"; // JvB added
 
 const WelcomeModal = () => {
   const dispatch = useDispatch();
@@ -29,6 +30,11 @@ const WelcomeModal = () => {
   const onClose = () => {
     const tilesetSampleFile = dataURLtoFile(tilesetSample, "tileset_sample.png");
     dispatch(addTileset("Example Tileset", [32, 32], TileManagerInstance.lastId, tilesetSampleFile));
+
+    // JvB added
+    const tilesetSampleFile2 = dataURLtoFile(tilesetSample2, "tilesets_deviant_milkian_1.png");
+    dispatch(addTileset("WorkAdventu.re Tileset", [32, 32], TileManagerInstance.lastId, tilesetSampleFile2));
+   
     dispatch(completeTextureLoading());
     dispatch(displaySidebar(true));
     setOpen(false);
